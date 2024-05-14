b2c030fcb72de585a21fafd866fada81193fab0a
Dovineowuor <owuordove@gmail.com>
2024-05-14 09:06:06 +0300

    Updates

--- dovetecenterprises/dovetecenterprises/settings.py
+++ dovetecenterprises/dovetecenterprises/settings.py
@@ -116,7 +116,8 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
-
+# AUTH USER MODEL
+AUTH_USER_MODEL = 'app.User'
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
