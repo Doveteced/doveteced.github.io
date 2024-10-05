d304f48abfba0139606cc7ed01e2a289d8c06a5d
dovineowuor <owuordove@gmail.com>
2024-10-05 21:03:49 +0300

    Refactor admin configuration for models in article/home/admin.py

--- article/home/admin.py
+++ article/home/admin.py
@@ -1,7 +1,25 @@
 from django.contrib import admin
+from .models import Article, Profile, Reply, Tag, Comment
 
-# Register your models here.
-from .models import Article, Profile
-
+# Register the Article model
 admin.site.register(Article)
-admin.site.register(Profile)
+
+# Admin configuration for the Profile model
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in Profile._meta.fields]
+
+# Admin configuration for the Tag model
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in Tag._meta.fields]
+
+# Admin configuration for the Comment model
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in Comment._meta.fields]
+
+# Admin configuration for the Reply model
+@admin.register(Reply)
+class ReplyAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in Reply._meta.fields]
