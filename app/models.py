884abb96b78d312cbb2aae2ff49d8d4863df73f9
Dovine K <70941806+Dovineowuor@users.noreply.github.com>
2025-01-15 02:01:59 +0300

    Add username field to User model with automatic generation logic

--- app/models.py
+++ app/models.py
@@ -11,4 +11,16 @@ class User(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='user')
+    username = models.CharField(max_length=100, unique=True, blank=True, null=True)
+
+    def save(self, *args, **kwargs):
+        if not self.username:
+            if self.email:
+                self.username = self.email.split('@')[0]
+            else:
+                self.username = (self.first_name + self.last_name).lower()
+        super(User, self).save(*args, **kwargs)
+    
+    def __str__(self):
+        return self.username
     
